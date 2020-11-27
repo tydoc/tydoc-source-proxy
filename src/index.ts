423c6f4aa8c3249457b7ef06b77352887d648bd5
Johannes Schickling <schickling.j@gmail.com>
2020-11-27 18:15:35 +0100

    Fixed vercel

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,18 @@
-// process.env['DEBUG'] = 'tydoc:*'
-
-import { NowRequest, NowResponse } from '@vercel/node'
 import { promises as fs } from 'fs'
 import * as path from 'path'
 import * as os from 'os'
 import * as request from 'request'
 import { downloadAndUnzip } from './download-unzip'
+// process.env['DEBUG'] = 'tydoc:*'
 import * as TyDoc from 'tydoc'
 
-export default async function (req: NowRequest, res: NowResponse) {
-  const { github, entrypoint } = req.query as {
-    github: string
-    entrypoint: string
-  }
-
-  if (!github) {
-    res.status(400).send(`No query param "github=[string]" provided`)
-    return
-  }
-
-  if (!entrypoint) {
-    res.status(400).send(`No query param "entrypoint=[string]" provided`)
-    return
-  }
+export async function proxy({
+  github,
+  entrypoint,
+}: {
+  github: string
+  entrypoint: string
+}): Promise<string> {
   const [, owner, name] = github.match(/^https:\/\/github\.com\/(.+?)\/(.+?)$/)!
   const { default_branch: defaultBranch } = await getJson<{
     default_branch: string
@@ -32,24 +22,19 @@ export default async function (req: NowRequest, res: NowResponse) {
     path.join(os.tmpdir(), `tydoc-source-proxy-${owner}-${name}-`),
   )
 
-  try {
-    await downloadAndUnzip(
-      `https://github.com/${owner}/${name}/tarball/${defaultBranch}`,
-      dir,
-    )
+  await downloadAndUnzip(
+    `https://github.com/${owner}/${name}/tarball/${defaultBranch}`,
+    dir,
+  )
 
-    const docs = TyDoc.fromProject({
-      entrypoints: [entrypoint],
-      readSettingsFromJSON: true,
-      haltOnDiagnostics: false,
-      prjDir: dir,
-    })
+  const docs = TyDoc.fromProject({
+    entrypoints: [entrypoint],
+    readSettingsFromJSON: true,
+    haltOnDiagnostics: false,
+    prjDir: dir,
+  })
 
-    res.send(JSON.stringify(docs))
-  } catch (e) {
-    console.error(e)
-    res.status(500).send(`Error occured: ${e}`)
-  }
+  return JSON.stringify(docs)
 }
 
 async function getJson<T>(url: string): Promise<T> {
